f1615a977d33c190713ab1832af77d98796c31c3
Sameer Srivastava <sameer.srivastava.17@mountblue.tech>
2021-11-17 20:57:22 +0530

    ajax loader done

---
+++ script/menu.js
@@ -0,0 +1,22 @@
+
+var div=document.getElementById('app');
+var req=new  XMLHttpRequest();
+req.open('GET','../script/menuitems.json');
+req.onload=()=>{
+var data=JSON.parse(req.responseText);
+  renderHtml(data);
+}
+req.send();
+
+function renderHtml(data){
+div.innerHTML=`<h1 class="special-heading>Specials</h1>
+${data.map((value)=>{
+return `
+<div class="special-item">
+<img class="specials-photos" src="${value.img}"</div>
+<h3>${value.name}</h3>
+`
+}).join(" ")}`;
+}
+
+ 
